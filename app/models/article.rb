3d58a451aee98030299882b4ec618714d5050b6d
Katarina <katarina.mursec@gmail.com>
2018-09-11 11:29:58 -0400

    create new action

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 3, maximum: 30}
-  validates :description, presence: true, length: {minimum: 20, maximum: 300}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
 end
